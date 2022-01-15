06560c9d2d360d49d2a76cc0d3924042fc0e1f30
Unnati <chhabraunnati324@gmail.com>
2022-01-15 10:55:50 +0530

    Update SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,7 +5,7 @@
                 <v-list-item class="mb-12">
                     <v-list-item-content>
                         <v-icon class="mb-2" large>fas fa-hand-holding-medical</v-icon>
-                        <v-list-item-subtitle align="center">Medi</v-list-item-subtitle>
+                        <v-list-item-subtitle align="center">Medical</v-list-item-subtitle>
                     </v-list-item-content>
                 </v-list-item>
             </v-list>
@@ -16,7 +16,7 @@
                 </v-list-item-content>
                 </v-list-item>
                 <v-list-item router to="/">
-                <v-badge color="pink" dot overlap>
+                <v-badge color="blue" dot overlap>
                         <v-list-item-content>
                             <v-icon class="mb-2">fas fa-user</v-icon>
                         </v-list-item-content>
